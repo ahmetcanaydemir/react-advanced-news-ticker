b3bf4674f98e5ad06899300e2838596e13bc15bf
Ahmet Can Aydemir <tusunami123@gmail.com>
2020-09-27 22:45:57 +0300

    Updates

---
+++ precache-manifest.fa5e9b6dde9bb9e7cb108fcbf85067ae.js
@@ -0,0 +1,514 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912cfc3a31eb8a0ed9fb0e05089006c1",
+    "url": "/react-advanced-news-ticker/index.html"
+  },
+  {
+    "revision": "c1ffcf052dea957cb860",
+    "url": "/react-advanced-news-ticker/static/css/8.b9ddd548.chunk.css"
+  },
+  {
+    "revision": "b2024c78ecbf0d260535",
+    "url": "/react-advanced-news-ticker/static/css/main.c8cf8136.chunk.css"
+  },
+  {
+    "revision": "9d3138bb7ec23566ff51",
+    "url": "/react-advanced-news-ticker/static/js/0.5976f406.chunk.js"
+  },
+  {
+    "revision": "2bbbb55d874ad8813566",
+    "url": "/react-advanced-news-ticker/static/js/1.25a048cf.chunk.js"
+  },
+  {
+    "revision": "ad5d8e4905fa2f7822a7",
+    "url": "/react-advanced-news-ticker/static/js/10.b7f4e81e.chunk.js"
+  },
+  {
+    "revision": "f4a0dad22d72734ac87b",
+    "url": "/react-advanced-news-ticker/static/js/100.d3f66aac.chunk.js"
+  },
+  {
+    "revision": "253c3c833a0bcce1f8bf",
+    "url": "/react-advanced-news-ticker/static/js/101.281eab03.chunk.js"
+  },
+  {
+    "revision": "c01d03eb0ee633638dd8",
+    "url": "/react-advanced-news-ticker/static/js/102.afc77296.chunk.js"
+  },
+  {
+    "revision": "7c3f4a52826c85a3b87c",
+    "url": "/react-advanced-news-ticker/static/js/103.5c3f52cb.chunk.js"
+  },
+  {
+    "revision": "abcdbda7669378713107",
+    "url": "/react-advanced-news-ticker/static/js/104.1d07711d.chunk.js"
+  },
+  {
+    "revision": "88119e27a2cda5895129",
+    "url": "/react-advanced-news-ticker/static/js/105.03aadb10.chunk.js"
+  },
+  {
+    "revision": "1592f0fe359c2052d516",
+    "url": "/react-advanced-news-ticker/static/js/106.72e6eab1.chunk.js"
+  },
+  {
+    "revision": "24dd70b872c907eb3c7d",
+    "url": "/react-advanced-news-ticker/static/js/107.208e0ccb.chunk.js"
+  },
+  {
+    "revision": "ed99746a83f45bc45644",
+    "url": "/react-advanced-news-ticker/static/js/108.dc0b7f95.chunk.js"
+  },
+  {
+    "revision": "fe34e0704b3e15643b14",
+    "url": "/react-advanced-news-ticker/static/js/109.f0026da3.chunk.js"
+  },
+  {
+    "revision": "648a3406a4e05e0c9e33",
+    "url": "/react-advanced-news-ticker/static/js/11.1d6632c3.chunk.js"
+  },
+  {
+    "revision": "968660af1861a150f02f",
+    "url": "/react-advanced-news-ticker/static/js/110.0eed8d94.chunk.js"
+  },
+  {
+    "revision": "8577ade415407f9788cf",
+    "url": "/react-advanced-news-ticker/static/js/111.0b8cf85e.chunk.js"
+  },
+  {
+    "revision": "069db59ba9de916540a4",
+    "url": "/react-advanced-news-ticker/static/js/112.abcb67d5.chunk.js"
+  },
+  {
+    "revision": "8591045a2d5c85461fcd",
+    "url": "/react-advanced-news-ticker/static/js/113.3a82ad91.chunk.js"
+  },
+  {
+    "revision": "3745fcb1ce295389a17d",
+    "url": "/react-advanced-news-ticker/static/js/114.f4297aae.chunk.js"
+  },
+  {
+    "revision": "b4fcd7573cd43893f67c",
+    "url": "/react-advanced-news-ticker/static/js/115.047fa5bd.chunk.js"
+  },
+  {
+    "revision": "2409ff548cbc9cda6ec8",
+    "url": "/react-advanced-news-ticker/static/js/116.add147d6.chunk.js"
+  },
+  {
+    "revision": "471a3daf88ec637c7c25",
+    "url": "/react-advanced-news-ticker/static/js/117.0f99e9c4.chunk.js"
+  },
+  {
+    "revision": "2aea596f74ecf4fa2b1b",
+    "url": "/react-advanced-news-ticker/static/js/118.e7f0964b.chunk.js"
+  },
+  {
+    "revision": "4bf8d83820e55de75409",
+    "url": "/react-advanced-news-ticker/static/js/119.21ff39a8.chunk.js"
+  },
+  {
+    "revision": "bbd2c91ac3a525940171",
+    "url": "/react-advanced-news-ticker/static/js/12.09947534.chunk.js"
+  },
+  {
+    "revision": "13affcaefe4b36f8ec6a",
+    "url": "/react-advanced-news-ticker/static/js/120.0d3b6bdb.chunk.js"
+  },
+  {
+    "revision": "4a101782f3389cea26ff",
+    "url": "/react-advanced-news-ticker/static/js/121.f3234a9f.chunk.js"
+  },
+  {
+    "revision": "e6aab50378bc46eaaf76",
+    "url": "/react-advanced-news-ticker/static/js/122.46c2ff48.chunk.js"
+  },
+  {
+    "revision": "5959b252b8650e75db37",
+    "url": "/react-advanced-news-ticker/static/js/123.3f542c97.chunk.js"
+  },
+  {
+    "revision": "f8734f55d6b53a9c6b7f",
+    "url": "/react-advanced-news-ticker/static/js/13.a86c1570.chunk.js"
+  },
+  {
+    "revision": "42ff0d20d5334c10f4d3",
+    "url": "/react-advanced-news-ticker/static/js/14.0ab5eece.chunk.js"
+  },
+  {
+    "revision": "c4a36e7ddf71fa2e0b67",
+    "url": "/react-advanced-news-ticker/static/js/15.5e896c90.chunk.js"
+  },
+  {
+    "revision": "afce163b89bf7beba2b9",
+    "url": "/react-advanced-news-ticker/static/js/16.bcab4f63.chunk.js"
+  },
+  {
+    "revision": "0bcef92e8b06c4d1b011",
+    "url": "/react-advanced-news-ticker/static/js/17.2614cb6f.chunk.js"
+  },
+  {
+    "revision": "159caac93b94e6b0d89c",
+    "url": "/react-advanced-news-ticker/static/js/18.b9a94371.chunk.js"
+  },
+  {
+    "revision": "913d666bdb0315c665a3",
+    "url": "/react-advanced-news-ticker/static/js/19.57f18549.chunk.js"
+  },
+  {
+    "revision": "27878ae138eaf37571d0",
+    "url": "/react-advanced-news-ticker/static/js/2.183d45d9.chunk.js"
+  },
+  {
+    "revision": "50a624ffe3771e773c50",
+    "url": "/react-advanced-news-ticker/static/js/20.ca7e93ac.chunk.js"
+  },
+  {
+    "revision": "df2d768be443e53ebffe",
+    "url": "/react-advanced-news-ticker/static/js/21.7319efb4.chunk.js"
+  },
+  {
+    "revision": "d51de3e693e6b24d1ad2",
+    "url": "/react-advanced-news-ticker/static/js/22.df698db6.chunk.js"
+  },
+  {
+    "revision": "236c7143f43659c08d12",
+    "url": "/react-advanced-news-ticker/static/js/23.630a7fcb.chunk.js"
+  },
+  {
+    "revision": "2cf103fc8b4ec382e1b8",
+    "url": "/react-advanced-news-ticker/static/js/24.7bcde65f.chunk.js"
+  },
+  {
+    "revision": "7abb4effb776d0cb28d6",
+    "url": "/react-advanced-news-ticker/static/js/25.2d37e9c0.chunk.js"
+  },
+  {
+    "revision": "1c923ac81ca36ade2867",
+    "url": "/react-advanced-news-ticker/static/js/26.2b25424a.chunk.js"
+  },
+  {
+    "revision": "4bb9b9ca133edf524310",
+    "url": "/react-advanced-news-ticker/static/js/27.47a891d2.chunk.js"
+  },
+  {
+    "revision": "4ae9c54ccf712f797e2e",
+    "url": "/react-advanced-news-ticker/static/js/28.2d2a8049.chunk.js"
+  },
+  {
+    "revision": "5e44de6e686177fcad17",
+    "url": "/react-advanced-news-ticker/static/js/29.155bd9bf.chunk.js"
+  },
+  {
+    "revision": "d082bd0059f9793c0d70",
+    "url": "/react-advanced-news-ticker/static/js/3.3e8d217f.chunk.js"
+  },
+  {
+    "revision": "032a3a47852b4cdbb39d",
+    "url": "/react-advanced-news-ticker/static/js/30.4c50d453.chunk.js"
+  },
+  {
+    "revision": "43c1c97b6e99bd5301b1",
+    "url": "/react-advanced-news-ticker/static/js/31.b6c9f444.chunk.js"
+  },
+  {
+    "revision": "1ac2fdb9b772a14d004a",
+    "url": "/react-advanced-news-ticker/static/js/32.7800038b.chunk.js"
+  },
+  {
+    "revision": "a1b8cd8b6601d0377672",
+    "url": "/react-advanced-news-ticker/static/js/33.f6cfa5e1.chunk.js"
+  },
+  {
+    "revision": "e1c67c5f4c52fb835a25",
+    "url": "/react-advanced-news-ticker/static/js/34.057980fc.chunk.js"
+  },
+  {
+    "revision": "9fe9967bfe712b792ed8",
+    "url": "/react-advanced-news-ticker/static/js/35.50053dda.chunk.js"
+  },
+  {
+    "revision": "d0aa6420dc2dc012b5f0",
+    "url": "/react-advanced-news-ticker/static/js/36.6ad3745e.chunk.js"
+  },
+  {
+    "revision": "8f170213072e50794fda",
+    "url": "/react-advanced-news-ticker/static/js/37.efedb53a.chunk.js"
+  },
+  {
+    "revision": "c10b5564bd52314f852c",
+    "url": "/react-advanced-news-ticker/static/js/38.bed39e02.chunk.js"
+  },
+  {
+    "revision": "31d8e238e8e1f94394d9",
+    "url": "/react-advanced-news-ticker/static/js/39.9476e352.chunk.js"
+  },
+  {
+    "revision": "8cc46b6ab81dd0da0909",
+    "url": "/react-advanced-news-ticker/static/js/4.f75041b7.chunk.js"
+  },
+  {
+    "revision": "a70ecd8f24a93f9d7dac",
+    "url": "/react-advanced-news-ticker/static/js/40.2b8a4f93.chunk.js"
+  },
+  {
+    "revision": "31744570f1f79c872b03",
+    "url": "/react-advanced-news-ticker/static/js/41.1799118b.chunk.js"
+  },
+  {
+    "revision": "bf10c6920f58cebbcb09",
+    "url": "/react-advanced-news-ticker/static/js/42.31bbffbd.chunk.js"
+  },
+  {
+    "revision": "097a5062aac005854895",
+    "url": "/react-advanced-news-ticker/static/js/43.d89d17f5.chunk.js"
+  },
+  {
+    "revision": "f08852efbe81f699bfae",
+    "url": "/react-advanced-news-ticker/static/js/44.9d573eaf.chunk.js"
+  },
+  {
+    "revision": "409392991afe569a57f6",
+    "url": "/react-advanced-news-ticker/static/js/45.3aaadf3b.chunk.js"
+  },
+  {
+    "revision": "f7d47852b0915cf02303",
+    "url": "/react-advanced-news-ticker/static/js/46.7adaf161.chunk.js"
+  },
+  {
+    "revision": "a5912935ed81ff295bd9",
+    "url": "/react-advanced-news-ticker/static/js/47.079eb577.chunk.js"
+  },
+  {
+    "revision": "0df0e38816d617939575",
+    "url": "/react-advanced-news-ticker/static/js/48.080ee15c.chunk.js"
+  },
+  {
+    "revision": "04e8f01e7f83b367bdb7",
+    "url": "/react-advanced-news-ticker/static/js/49.f9b74144.chunk.js"
+  },
+  {
+    "revision": "032612a2d2259bd9abd6",
+    "url": "/react-advanced-news-ticker/static/js/5.7f2f96f5.chunk.js"
+  },
+  {
+    "revision": "bf524b5df30b3625647a",
+    "url": "/react-advanced-news-ticker/static/js/50.770ff1d8.chunk.js"
+  },
+  {
+    "revision": "013301038adda40d10eb",
+    "url": "/react-advanced-news-ticker/static/js/51.63ff765f.chunk.js"
+  },
+  {
+    "revision": "26970cacb332da13ccea",
+    "url": "/react-advanced-news-ticker/static/js/52.1f78dae9.chunk.js"
+  },
+  {
+    "revision": "5afc9e84fbf2511368ff",
+    "url": "/react-advanced-news-ticker/static/js/53.cd060847.chunk.js"
+  },
+  {
+    "revision": "e7ef2339e5a59fd86f7e",
+    "url": "/react-advanced-news-ticker/static/js/54.55935e52.chunk.js"
+  },
+  {
+    "revision": "e31e93bdda2204656772",
+    "url": "/react-advanced-news-ticker/static/js/55.0392fe16.chunk.js"
+  },
+  {
+    "revision": "fe73cfb3586d78f7e9f9",
+    "url": "/react-advanced-news-ticker/static/js/56.45be9c0f.chunk.js"
+  },
+  {
+    "revision": "cfeee5c2d8b0a732cb9c",
+    "url": "/react-advanced-news-ticker/static/js/57.87dd597b.chunk.js"
+  },
+  {
+    "revision": "1150c7eb335b654a6474",
+    "url": "/react-advanced-news-ticker/static/js/58.c9a517c7.chunk.js"
+  },
+  {
+    "revision": "c264223491954b42d43e",
+    "url": "/react-advanced-news-ticker/static/js/59.3ab95ab3.chunk.js"
+  },
+  {
+    "revision": "b791c642abd8f5a0971b",
+    "url": "/react-advanced-news-ticker/static/js/60.14d9fdbb.chunk.js"
+  },
+  {
+    "revision": "a394b9d59cc8df89a8a2",
+    "url": "/react-advanced-news-ticker/static/js/61.1d4c1f26.chunk.js"
+  },
+  {
+    "revision": "c87ad09dc3cfd35dd71d",
+    "url": "/react-advanced-news-ticker/static/js/62.a0237189.chunk.js"
+  },
+  {
+    "revision": "fb4e50b39a25cc206a85",
+    "url": "/react-advanced-news-ticker/static/js/63.74501212.chunk.js"
+  },
+  {
+    "revision": "16c32ae419e700833033",
+    "url": "/react-advanced-news-ticker/static/js/64.de4b3089.chunk.js"
+  },
+  {
+    "revision": "116c88089c985c664bd2",
+    "url": "/react-advanced-news-ticker/static/js/65.32aedb99.chunk.js"
+  },
+  {
+    "revision": "1f4684c5a70aea277d8c",
+    "url": "/react-advanced-news-ticker/static/js/66.cee1e80e.chunk.js"
+  },
+  {
+    "revision": "b2ef621deb257dce87b0",
+    "url": "/react-advanced-news-ticker/static/js/67.f681f195.chunk.js"
+  },
+  {
+    "revision": "eaef8f38ee47159d8ff7",
+    "url": "/react-advanced-news-ticker/static/js/68.b25d3606.chunk.js"
+  },
+  {
+    "revision": "443c6b9629d75919fdab",
+    "url": "/react-advanced-news-ticker/static/js/69.eb82a8c1.chunk.js"
+  },
+  {
+    "revision": "f6a6d14bce2ac27047c6",
+    "url": "/react-advanced-news-ticker/static/js/70.8d513ea2.chunk.js"
+  },
+  {
+    "revision": "7347134e8fd1a4c27fa9",
+    "url": "/react-advanced-news-ticker/static/js/71.2560ff2e.chunk.js"
+  },
+  {
+    "revision": "c6bdea7067798712cbe9",
+    "url": "/react-advanced-news-ticker/static/js/72.fea4d1fa.chunk.js"
+  },
+  {
+    "revision": "188cacbe539f2328d903",
+    "url": "/react-advanced-news-ticker/static/js/73.4f73b017.chunk.js"
+  },
+  {
+    "revision": "e9cccbaa15783e9c22d9",
+    "url": "/react-advanced-news-ticker/static/js/74.089ab55d.chunk.js"
+  },
+  {
+    "revision": "85a4fb4950b83d190a9c",
+    "url": "/react-advanced-news-ticker/static/js/75.14f01e53.chunk.js"
+  },
+  {
+    "revision": "a9d292cd911f62e98296",
+    "url": "/react-advanced-news-ticker/static/js/76.e2809a28.chunk.js"
+  },
+  {
+    "revision": "42cec026cc5a65d4c9f6",
+    "url": "/react-advanced-news-ticker/static/js/77.4b652a42.chunk.js"
+  },
+  {
+    "revision": "7022e5752626da6926a5",
+    "url": "/react-advanced-news-ticker/static/js/78.46c23f82.chunk.js"
+  },
+  {
+    "revision": "796dc3f6d8254f2f1bad",
+    "url": "/react-advanced-news-ticker/static/js/79.5573c266.chunk.js"
+  },
+  {
+    "revision": "c1ffcf052dea957cb860",
+    "url": "/react-advanced-news-ticker/static/js/8.faf3d213.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-advanced-news-ticker/static/js/8.faf3d213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a44164281b012badf6",
+    "url": "/react-advanced-news-ticker/static/js/80.e7f3e6b4.chunk.js"
+  },
+  {
+    "revision": "9b21c7edd264f9bc21f5",
+    "url": "/react-advanced-news-ticker/static/js/81.f3f8c57c.chunk.js"
+  },
+  {
+    "revision": "a9a2b34ab091f7a1a219",
+    "url": "/react-advanced-news-ticker/static/js/82.44686790.chunk.js"
+  },
+  {
+    "revision": "028457c72600351cde5d",
+    "url": "/react-advanced-news-ticker/static/js/83.1e1a2241.chunk.js"
+  },
+  {
+    "revision": "a0e0fb9959af142c813a",
+    "url": "/react-advanced-news-ticker/static/js/84.a1b5e24f.chunk.js"
+  },
+  {
+    "revision": "886b5449101894864a25",
+    "url": "/react-advanced-news-ticker/static/js/85.0d528ff7.chunk.js"
+  },
+  {
+    "revision": "6eac24ca3b49532d5f14",
+    "url": "/react-advanced-news-ticker/static/js/86.7bff830a.chunk.js"
+  },
+  {
+    "revision": "e8d52d8b7fd9df26d324",
+    "url": "/react-advanced-news-ticker/static/js/87.7f7e7082.chunk.js"
+  },
+  {
+    "revision": "4772fd39667b2d245615",
+    "url": "/react-advanced-news-ticker/static/js/88.9a502b5b.chunk.js"
+  },
+  {
+    "revision": "14b6d97c35f2681a1a95",
+    "url": "/react-advanced-news-ticker/static/js/89.227399ae.chunk.js"
+  },
+  {
+    "revision": "f8d460b347b9038eb873",
+    "url": "/react-advanced-news-ticker/static/js/9.03ceab99.chunk.js"
+  },
+  {
+    "revision": "a0183682034db9572566",
+    "url": "/react-advanced-news-ticker/static/js/90.d1f2e394.chunk.js"
+  },
+  {
+    "revision": "a068e9d7d88f49ed1875",
+    "url": "/react-advanced-news-ticker/static/js/91.76569898.chunk.js"
+  },
+  {
+    "revision": "90ec0303622e7037c5d1",
+    "url": "/react-advanced-news-ticker/static/js/92.6552a740.chunk.js"
+  },
+  {
+    "revision": "c1ecff01b3da89606ddf",
+    "url": "/react-advanced-news-ticker/static/js/93.6b9bc2a7.chunk.js"
+  },
+  {
+    "revision": "437ace28562b5e500f97",
+    "url": "/react-advanced-news-ticker/static/js/94.11785f3b.chunk.js"
+  },
+  {
+    "revision": "0d85e7511b6f6600bfe2",
+    "url": "/react-advanced-news-ticker/static/js/95.c40b0248.chunk.js"
+  },
+  {
+    "revision": "0401dfb54da10d8be68b",
+    "url": "/react-advanced-news-ticker/static/js/96.ac566a4a.chunk.js"
+  },
+  {
+    "revision": "8ac630f77b2c9fb2a77e",
+    "url": "/react-advanced-news-ticker/static/js/97.68d072c4.chunk.js"
+  },
+  {
+    "revision": "92831b4645eac7cb1cfe",
+    "url": "/react-advanced-news-ticker/static/js/98.5f35d2e6.chunk.js"
+  },
+  {
+    "revision": "67fc3d44448f30ea784c",
+    "url": "/react-advanced-news-ticker/static/js/99.fa2d5236.chunk.js"
+  },
+  {
+    "revision": "b2024c78ecbf0d260535",
+    "url": "/react-advanced-news-ticker/static/js/main.d36e82d2.chunk.js"
+  },
+  {
+    "revision": "9c5ba32df824e1e7732c",
+    "url": "/react-advanced-news-ticker/static/js/runtime-main.773cf334.js"
+  }
+]);
